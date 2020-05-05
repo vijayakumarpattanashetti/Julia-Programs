6f55fe98b16383e0aebfe6f15f52881b68f2a5e2
Vijayakumar Pattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2020-05-06 03:50:22 +0530

    Update PLL.m

--- ADC/Others/PLL.m
+++ ADC/Others/PLL.m
@@ -1,32 +1,32 @@
 clc; clear all; close all; 
-f=1000;%Carrier frequency 
-fs=100000;%Sample frequency
-N=5000;%Number of samples
+f=2500;%Carrier frequency 
+fs=200000;%Sample frequency
+N=8000;%Number of samples
 Ts=1/fs;
 t=(0:Ts:(N*Ts)- Ts);
-f1=100;%Modulating frequency
+f1=150;%Modulating frequency
 msg=sin(2*pi*f1*t);
-kf=.0628;%Modulation index
-%Create the real and imaginary parts of a CW modulated carrier to be tracked.
+kf=.07;%Modulation index
+%modulated carrier to be tracked
 Signal=exp(j*(2*pi*f*t+2*pi*kf*cumsum(msg)));%Modulated carrier
 Signal1=exp(j*(2*pi*f*t));%Unmodulated carrier
 %Initilize PLL Loop 
-phi_hat(1)=30; 
+phi(1)=30; 
 e(1)=0; 
-phd_output(1)=0; 
+output(1)=0; 
 vco(1)=0; 
 %Define Loop Filter parameters(Sets damping)
-kp=0.15; %Proportional constant 
-ki=0.1; %Integrator constant 
+kp=0.12; %Proportional constant 
+ki=0.09; %Integrator constant 
 %PLL implementation 
 for n=2:length(Signal) 
-vco(n)=conj(exp(j*(2*pi*n*f/fs+phi_hat(n-1))));%Compute VCO 
-phd_output(n)=imag(Signal(n)*vco(n));%Complex multiply VCO x Signal input 
-e(n)=e(n-1)+(kp+ki)*phd_output(n)-ki*phd_output(n-1);%Filter integrator 
-phi_hat(n)=phi_hat(n-1)+e(n);%Update VCO 
+vco(n)=conj(exp(j*(2*pi*n*f/fs+phi(n-1))));%VCO O/P 
+output(n)=imag(Signal(n)*vco(n));%VCO x Signal input 
+e(n)=e(n-1)+(kp+ki)*output(n)-ki*output(n-1);%Filter integrator 
+phi(n)=phi(n-1)+e(n);%Update VCO 
 end; 
 startplot = 1;
-endplot = 1000;
+endplot = 2500;
 subplot(3,2,1);
 plot(t(startplot:endplot), msg(startplot:endplot));
 title('Signal');
